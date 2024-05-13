afb522177edcd60380ca595b5cf841c9b2a8efd7
rihil <rihil@10.48.35.230>
2024-05-13 13:59:13 -0400

    Initial_Commit

---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author rihil
+ */
+public class StudentList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Student s1 = new Student();
+        s1.setName("Rihil");
+        Student s2 = new Student();
+        s2.setName("Parth");
+        Student s3 = new Student();
+        s3.setName("dev");
+        System.out.println(s1.getName());        
+        System.out.println(s2.getName());
+        System.out.println(s3.getName());
+    }
+    
+}
